cdb3777029946c0e693878de9626f10b8abc3fd4
Ooky <blki@hotmail.com>
2016-06-23 15:38:04 +0200

    Added Constants, changed DesktopLauncher

---
+++ core/src/com/coldpixel/sparkle/Constants.java
@@ -0,0 +1,15 @@
+package com.coldpixel.sparkle;
+
+/**
+ *
+ * @author Ooky <blki at hotmail dot com>
+ */
+public class Constants {
+
+    public static final int WINDOW_WIDTH = 1024;
+    public static final int WINDOW_HEIGTH = 512 + 128;
+    public static final String GAMENAME = "Coldpixel - Alpha";
+    public static final boolean RESZIABLE = false;
+    public static final String FAVICON = "Graphics/Icon/Icon.png";
+    public static final boolean MAX_FPS = false;
+}
--- desktop/src/com/coldpixel/sparkle/desktop/DesktopLauncher.java
+++ desktop/src/com/coldpixel/sparkle/desktop/DesktopLauncher.java
@@ -1,12 +1,30 @@
 package com.coldpixel.sparkle.desktop;
 
+import com.badlogic.gdx.Files;
+import com.coldpixel.sparkle.Constants;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.coldpixel.sparkle.main;
+import com.coldpixel.sparkle.Main;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new main(), config);
-	}
+
+    public static void main(String[] arg) {
+
+        LwjglApplicationConfiguration config;
+        config = new LwjglApplicationConfiguration();
+
+        config.width = Constants.WINDOW_WIDTH;
+        config.height = Constants.WINDOW_HEIGTH;
+        config.title = Constants.GAMENAME;
+        config.resizable = Constants.RESZIABLE;
+        config.addIcon(Constants.FAVICON, Files.FileType.Internal);
+
+        if (Constants.MAX_FPS) {
+            //Shows the "real" fps, 0 disables throttling 
+            config.vSyncEnabled = false;
+            config.foregroundFPS = 0;
+            config.backgroundFPS = 0;
+        }
+        new LwjglApplication(new Main(), config);
+    }
 }
