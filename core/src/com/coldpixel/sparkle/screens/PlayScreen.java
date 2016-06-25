37b995af079d94c9f3fd699eedef0aaa5271a10a
Ooky <blki@hotmail.com>
2016-06-25 20:13:05 +0200

    Added Viewport, Cam,

--- core/src/com/coldpixel/sparkle/screens/PlayScreen.java
+++ core/src/com/coldpixel/sparkle/screens/PlayScreen.java
@@ -3,7 +3,11 @@ package com.coldpixel.sparkle.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.coldpixel.sparkle.Constants;
 import com.coldpixel.sparkle.Main;
 
 /**
@@ -17,13 +21,17 @@ public class PlayScreen implements Screen {
 //==============================================================================   
     private Main main;
     Texture texture;
+    private OrthographicCamera cam;
+    private Viewport gamePort;
     
 //==============================================================================
 //Methods
 //==============================================================================
     public PlayScreen(Main main) {
         this.main = main;
-        texture = new Texture("Graphics/Terrain/Terrain.png");
+        texture = new Texture("Graphics/Character/Character.png");
+        cam = new OrthographicCamera();
+        gamePort = new StretchViewport(Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGTH,cam);
     }
 
     @Override
@@ -32,15 +40,18 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float f) {
+        cam.update();
         Gdx.gl.glClearColor(42/255f, 47/255f, 48/255f, 1);//0-1, Float.
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        main.batch.setProjectionMatrix(cam.combined);
         main.batch.begin();
         main.batch.draw(texture, 0, 0);
         main.batch.end();
     }
 
     @Override
-    public void resize(int i, int i1) {
+    public void resize(int width, int height) {
+        gamePort.update(width, height); //Adjust the Viewport
     }
 
     @Override
